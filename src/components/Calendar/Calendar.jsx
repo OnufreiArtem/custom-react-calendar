96a51b27b4e137ef1094cd1ee2c7d2c1ae7ea127
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-12 14:14:21 +0300

    Some shit

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,12 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import DayCell from "./DayCell"
+import DayCell from "./DayCell";
 
-function Calendar({date}) {
-
+function Calendar({ date }) {
     const NUMBER_OF_ROWS = 6;
 
     const [current, setCurrent] = useState(new Date(date.getTime()));
@@ -14,8 +13,19 @@ function Calendar({date}) {
 
     const [selectedDate, setSelectedDate] = useState(undefined);
 
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+    const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
     ];
 
     const weekDayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -24,20 +34,20 @@ function Calendar({date}) {
         box-shadow: 0 0 10px #0005;
         padding: 2rem 1rem;
         border-radius: 5px;
-    `
+    `;
 
     const CalendarHeader = styled.div`
         display: flex;
         justify-content: space-between;
         align-items: center;
         margin-bottom: 20px;
-    `
+    `;
 
     const CalendarTitle = styled.span`
         font-size: 2rem;
         font-weight: bold;
-        font-family: 'Dancing Script', cursive;
-    `
+        font-family: "Dancing Script", cursive;
+    `;
     const MoveArrow = styled.div`
         aspect-ratio: 1/1;
         font-size: 2rem;
@@ -45,146 +55,106 @@ function Calendar({date}) {
         display: flex;
         justify-content: center;
         align-items: center;
-        transition: .3s all;
+        transition: 0.3s all;
 
         &:hover {
             background-color: #ccc;
         }
-    `
+    `;
 
     const DaysContainer = styled.div`
         display: grid;
         grid-template-columns: repeat(7, 1fr);
         grid-column-gap: 10px;
         text-align: center;
-    `
+    `;
 
     const WeekDayCell = styled.span`
         margin-bottom: 20px;
-    `
-/*
-    const DateCell = styled.span`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        aspect-ratio: 1/1;
-        padding: 5px;
-        font-size: 1rem;
-        transition: .3s all;
-        color: ${props => props.out ? "#ccc" : "#40394a"};
-        cursor: pointer;
+    `;
 
-        ${
-            props => {
-                if(props.selected) return `background-color: red`;
-            }
-        }
-        
-        ${
-            props => {
-                if(!props.out) return `
-                    &:hover {
-                        background-color: #ccc;
-                    }
-                `
-            }
-        }
+    const SelectionControl = styled.div``;
 
-    `
-*/
-    const SelectionControl = styled.div`
-    
-    `
-
-    const CalendarFooter = styled.div`
-    
-    `
+    const CalendarFooter = styled.div``;
 
     const getAllDates = (date) => {
         let mDate = new Date(date.getTime());
         mDate.setDate(0);
         let daysBefore = mDate.getDay();
         mDate.setDate(mDate.getDate() - daysBefore);
-        return new Array(7*NUMBER_OF_ROWS).fill(0).map( () => {
+        return new Array(7 * NUMBER_OF_ROWS).fill(0).map(() => {
             mDate.setDate(mDate.getDate() + 1);
             return new Date(mDate.getTime());
         });
-    }
-
-    const ydmEquals = (date1, date2) => {
-        return date1.getDate() === date2.getDate() &&
-         date1.getFullYear() === date2.getFullYear() &&
-         date1.getMonth() == date2.getMonth();
-    }
-
-    const movePointerLeft = _ => {
-        let tempDate = new Date(pointer.getTime());
-        tempDate.setMonth(tempDate.getMonth() - 1);
-        setPointer(tempDate)
     };
 
-    const movePointerRight = _ => {
+    const ydmEquals = (date1, date2) => {
+        return (
+            date1?.getDate() === date2?.getDate() &&
+            date1?.getFullYear() === date2?.getFullYear() &&
+            date1?.getMonth() === date2?.getMonth()
+        );
+    };
+
+    const movePointerLeft = (_) => {
+        let tempDate = new Date(pointer.getTime());
+        tempDate.setMonth(tempDate.getMonth() - 1);
+        setPointer(tempDate);
+    };
+
+    const movePointerRight = (_) => {
         let tempDate = new Date(pointer.getTime());
         tempDate.setMonth(tempDate.getMonth() + 1);
-        setPointer(tempDate)
-        
-    } 
+        setPointer(tempDate);
+    };
 
-    const onDayClick = date => {
+    const onDayClick = (date) => {
+        console.log("Here")
         setSelectedDate(date);
-    }
+    };
 
-    /*
-    {
-                    getAllDates(pointer).map((day) => {
-                        let result = undefined;
-                        if(day.getMonth() !== pointer.getMonth()) result = <DateCell out key={`out_${day.getDate()}`}>{day.getDate()}</DateCell>
-                        else result = <DateCell key={`in_${day.getDate()}`} onClick={() => onDayClick(day) }>{day.getDate()}</DateCell>
-                        return result;
-                    })
-                }
-    */
 
     return (
-       <CalendarContainer>
+        <CalendarContainer>
             <CalendarHeader>
-                <MoveArrow onClick={() => movePointerLeft()}><i className="fas fa-chevron-left"  ></i></MoveArrow>
-                <CalendarTitle>{`${monthNames[pointer.getMonth()]} ${pointer.getFullYear()}`}</CalendarTitle>
-                <MoveArrow onClick={() => movePointerRight()}><i className="fas fa-chevron-right"></i></MoveArrow>
+                <MoveArrow onClick={() => movePointerLeft()}>
+                    <i className="fas fa-chevron-left"></i>
+                </MoveArrow>
+                <CalendarTitle>{`${
+                    monthNames[pointer.getMonth()]
+                } ${pointer.getFullYear()}`}</CalendarTitle>
+                <MoveArrow onClick={() => movePointerRight()}>
+                    <i className="fas fa-chevron-right"></i>
+                </MoveArrow>
             </CalendarHeader>
             <DaysContainer>
-                {
-                    weekDayNames.map(day => <WeekDayCell>{day.toUpperCase()}</WeekDayCell>)
-                }
+                {weekDayNames.map((day) => (
+                    <WeekDayCell>{day.toUpperCase()}</WeekDayCell>
+                ))}
 
                 {
-                    getAllDates(pointer).map((day) =>
-                         <DayCell dayText={day.getDate()} isIn={day.getMonth() === pointer.getMonth()} isSelected={ydmEquals(day, pointer)}/>
-                    )
-                    
+                    getAllDates(pointer).map((day, index) => (
+                        <DayCell
+                            key={`dayCell_${index}`}
+                            day={day}
+                            isIn={day.getMonth() === pointer.getMonth()}
+                            isSelected={ydmEquals(day, selectedDate)}
+                            onDayClick={(d) => onDayClick(d)}
+                        />
+                    ))
                 }
-
+                
             </DaysContainer>
 
-            <SelectionControl>
-                {
-                    <span>selectedDate</span>
-                }
-            </SelectionControl>
+            <SelectionControl>{<span>selectedDate</span>}</SelectionControl>
 
-            <CalendarFooter>
-
-            </CalendarFooter>
-
-       </CalendarContainer>
-    )
+            <CalendarFooter></CalendarFooter>
+        </CalendarContainer>
+    );
 }
 
 Calendar.prototype = {
     date: PropTypes.date,
-}
-
+};
 
 export default Calendar;
-
-
--- src/components/Calendar/DayCell.jsx
+++ src/components/Calendar/DayCell.jsx
@@ -1,78 +1,75 @@
-import React  from "react";
+import React from "react";
 
-import PropTypes from 'prop-types';
-import styled from "styled-components";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
-import { useState } from "react/cjs/react.development";
+import { useState, useEffect } from "react/cjs/react.development";
 
-let defaultColorScheme = {
-    inColor: "#40394a",
-    inColorSelected: "#red",
-    outColor: "#ccc",
-    outColorSelected: "#6a686e",
-    inToday: "",
-    outToday: "",
-    inRange: "",
-    outRange: "",
-}
-
-export default function DayCell({dayText=0, isIn=true, isSelected=false, inRange=false, isToday=false, colorScheme=defaultColorScheme}) {
-
-    const [dayIn, setIsIn] = useState(isIn);
-    const [daySelected, setIsSelected] = useState(isSelected);
-    const [dayToday, setIsToday] = useState(isToday);
-    const [dayRange, setInRange] = useState(inRange);
-
-    const DateCellContainer = styled.span`
+const BasicCell = styled.span`
     display: flex;
     justify-content: center;
     align-items: center;
     aspect-ratio: 1/1;
     padding: 5px;
     font-size: 1rem;
-    transition: .3s all;
-    color: ${() => {
-        if(dayIn) {
-            return daySelected ? colorScheme.inColorSelected : colorScheme.inColor
-        } else {
-            return daySelected ? colorScheme.outColorSelected : colorScheme.outColor
-        }
-    }};
+    transition: 0.3s all;
+    color: #40394a;
+    background-color: transparent;
+`;
+
+const InCell = styled(BasicCell)`
     cursor: pointer;
 
     &:hover {
-        background-color: ${colorScheme.outColor}
+        background-color: #ccc;
+    }
+`;
+
+const OutCell = styled(BasicCell)`
+    color: #ccc;
+`;
+
+const SelectedInCell = styled(InCell)`
+    color: red;
+    box-shadow: 0 0 5px #0005;
+`
+const SelectedOutCell = styled(OutCell)`
+    background-color: #ccc3
+`
+
+export default function DayCell({
+    day = 0,
+    isIn = true,
+    isSelected = false,
+    inRange = false,
+    isToday = false,
+    onDayClicked = (day) => {},
+}) {
+    const [daySelected, setIsSelected] = useState(isSelected);
+
+
+    const getCell = (selected) => {
+        let date = day.getDate();
+        if(isIn && selected) return <SelectedInCell onClick={() => onDayClicked(day)}>{date}</SelectedInCell>
+        else if(isIn && !selected) return <InCell onClick={() => onDayClicked(day)}>{date}</InCell>
+        else if(!isIn && selected) return <SelectedOutCell>{date}</SelectedOutCell>
+        if(!isIn && !selected) return <OutCell>{date}</OutCell>
     }
 
-    `
+    return (
+        <>
+            {
+                getCell(daySelected)
+            }
 
-    return(
-        <DateCellContainer onClick={() => { if(dayIn) setIsSelected(!isSelected) }}>
-            {dayText}
-        </DateCellContainer>
+        </>
     );
-
 }
 
-
 DayCell.prototype = {
-    dayText: PropTypes.number,
-    isIn: PropTypes.bool, 
+    day: PropTypes.object,
+    isIn: PropTypes.bool,
     isSelected: PropTypes.bool,
     inRange: PropTypes.bool,
     isToday: PropTypes.bool,
-
-    colorScheme: PropTypes.object = {
-        inColor: PropTypes.string,
-        inColorSelected: PropTypes.string,
-        outColor: PropTypes.string,
-        outColorSelected: PropTypes.string,
-        inToday: PropTypes.string,
-        outToday: PropTypes.string,
-        inRange: PropTypes.string,
-        outRange: PropTypes.string,
-    },
-}
-
-
-
+};
