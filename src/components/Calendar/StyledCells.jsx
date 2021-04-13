0d08b885b701b9e1dec266ce22bd9937a9b89108
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-13 09:10:59 +0300

    Changed styles

--- src/components/Calendar/StyledCells.jsx
+++ src/components/Calendar/StyledCells.jsx
@@ -1,4 +1,4 @@
-import styled, {keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const popup = keyframes`
   from {
@@ -11,56 +11,66 @@ export const popup = keyframes`
 `;
 
 export const WeekDayCell = styled.span`
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
 export const BasicCell = styled.span`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    aspect-ratio: 1/1;
-    padding: px;
-    font-size: 1rem;
-    transition: 0.3s all;
-    color: #40394a;
-    background-color: transparent;
-    user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  aspect-ratio: 1/1;
+  padding: px;
+  font-size: 1rem;
+  transition: 0.3s all;
+  color: #40394a;
+  background-color: transparent;
+  user-select: none;
 `;
 
 export const InCell = styled(BasicCell)`
-    cursor: pointer;
+  cursor: pointer;
 
-    &:hover {
-        background-color: #ccc;
-    }
+  &:hover {
+    background-color: #ccc;
+  }
 `;
 
 export const OutCell = styled(BasicCell)`
-    color: #ccc;
+  color: #ccc;
 `;
 
-export const RangeInCell = styled(InCell)`  
-    color: #0e49b5;
-    background-color: #0e49b555;
-    font-weight: bold;
-    border-radius: 999px;
-    animation: ${popup} .15s linear;
-`
+export const RangeInCell = styled(InCell)`
+  color: #0e49b5;
+  background-color: #0e49b555;
+  font-weight: bold;
+  border-radius: 999px;
+  animation: ${popup} 0.15s linear;
 
-export const RangeOutCell = styled(OutCell)`
-    color: #fff;
-    background-color: #0e49b522;
-    border-radius: 999px;
-    animation: ${popup} .15s linear;
-`
-
-export const SelectedInCell = styled(InCell)`
+  &:hover {
     color: #fff;
     background-color: #153e90;
-    box-shadow: 0 0 5px #0005;
-    animation: ${popup} .15s linear;
-`
+  }
+`;
+
+export const RangeOutCell = styled(OutCell)`
+  color: #fff;
+  background-color: #0e49b522;
+  border-radius: 999px;
+  animation: ${popup} 0.15s linear;
+`;
+
+export const SelectedInCell = styled(InCell)`
+  color: #fff;
+  background-color: #153e90;
+  box-shadow: 0 0 5px #0005;
+  animation: ${popup} 0.15s linear;
+
+  &:hover {
+    background-color: #153e90;
+  }
+`;
 
 export const SelectedOutCell = styled(OutCell)`
-    background-color: #153e90AA
-`
+  color: #fff;
+  background-color: #153e90aa;
+`;
